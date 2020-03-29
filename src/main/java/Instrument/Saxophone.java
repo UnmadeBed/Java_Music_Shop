c23ec9d7acf624d827c9fce6da0734ed532b9ad6
unmadebed <kevin.archer@live.com>
2020-03-29 23:11:11 +0100

    add Saxophone class instance vars and extend superclass

--- src/main/java/Instrument/Saxophone.java
+++ src/main/java/Instrument/Saxophone.java
@@ -1,4 +1,14 @@
 package Instrument;
 
-public class Saxophone {
+public class Saxophone extends Instrument {
+    private String size;
+
+    public Saxophone(String type, String material, String colour) {
+        super(type, material, colour);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
 }
