79e2be15da2edb254f5e832a3e709160ed36cca5
unmadebed <kevin.archer@live.com>
2020-03-29 23:00:55 +0100

    add Drums class instance vars and extend superclass

--- src/main/java/Instrument/Bass.java
+++ src/main/java/Instrument/Bass.java
@@ -2,13 +2,19 @@ package Instrument;
 
 public class Bass extends Instrument {
     private int numberOfStrings;
+    private String effectsPedal;
 
     public Bass(String type, String material, String colour) {
         super(type, material, colour);
                 this.numberOfStrings = numberOfStrings;
+                this.effectsPedal = effectsPedal;
     }
 
     public int getNumberOfStrings() {
         return numberOfStrings;
+    }
+
+    public String getEffectsPedal() {
+        return effectsPedal;
     }
 }
--- src/main/java/Instrument/Drums.java
+++ src/main/java/Instrument/Drums.java
@@ -1,4 +1,20 @@
 package Instrument;
 
-public class Drums {
+public class Drums extends Instrument {
+    private int numberOfCymbals;
+    private boolean hasSeat;
+
+    public Drums(String type, String material, String colour) {
+        super(type, material, colour);
+        this.numberOfCymbals = numberOfCymbals;
+        this.hasSeat = hasSeat;
+    }
+
+    public int getNumberOfCymbals() {
+        return numberOfCymbals;
+    }
+
+    public boolean hasSeat() {
+        return hasSeat;
+    }
 }
