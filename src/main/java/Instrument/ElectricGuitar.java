3751d94f265009025106be278bfd6992898e7aa7
unmadebed <kevin.archer@live.com>
2020-03-29 23:04:43 +0100

    add ElectricGuitar  class instance vars and extend superclass

--- src/main/java/Instrument/ElectricGuitar.java
+++ src/main/java/Instrument/ElectricGuitar.java
@@ -1,4 +1,26 @@
 package Instrument;
 
-public class ElectricGuitar {
+public class ElectricGuitar extends Instrument{
+    private int numberOfStrings;
+    private String effectsPedal;
+    private boolean whammy;
+
+    public ElectricGuitar(String type, String material, String colour) {
+        super(type, material, colour);
+        this.numberOfStrings = numberOfStrings;
+        this.effectsPedal = effectsPedal;
+        this.whammy = whammy;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public String getEffectsPedal() {
+        return effectsPedal;
+    }
+
+    public boolean hasWhammy() {
+        return whammy;
+    }
 }
