b3dc5e66ecb64cac8ac870a08ce02b5718efbecf
unmadebed <kevin.archer@live.com>
2020-03-29 22:34:03 +0100

    add Bass class instance vars and extend superclass

--- src/main/java/Instrument/Bass.java
+++ src/main/java/Instrument/Bass.java
@@ -1,5 +1,14 @@
 package Instrument;
 
 public class Bass extends Instrument {
+    private int numberOfStrings;
 
+    public Bass(String type, String material, String colour) {
+        super(type, material, colour);
+                this.numberOfStrings = numberOfStrings;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
 }
