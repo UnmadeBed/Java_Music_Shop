6d8616cd768780bbbae895f991d42f0bc2390a9e
unmadebed <kevin.archer@live.com>
2020-03-30 08:57:19 +0100

    MVP

--- src/main/java/MusicShop/Shop.java
+++ src/main/java/MusicShop/Shop.java
@@ -1,4 +1,23 @@
 package MusicShop;
 
+import Behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public void addStock(ISell item){
+        this.stock.add(item);
+    }
+
+    public void removeStock(ISell item){
+        this.stock.remove(item);
+    }
+
 }
